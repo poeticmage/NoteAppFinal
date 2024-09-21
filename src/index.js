13260424e45e1f85f75cc7e9f803702aa303f53d
Priyadarshi Mukhopadhyay <158742737+poeticmage@users.noreply.github.com>
2024-09-21 11:45:15 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./../public/styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
